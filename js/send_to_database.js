a398356389b329bac0162a431cb21db0204ca84f
MrRedHerring <ssfvessfve@163.com>
2019-03-14 02:17:47 +0000

    20190314

--- js/send_to_database.js
+++ js/send_to_database.js
@@ -223,6 +223,39 @@ function upload_pdf() {
 	});
 }
 
+function submit_pdf_info() {
+	pdf_name = document.getElementById("pdf_name").value;
+	crop_len = document.getElementById("crop_len").value;
+	search_name = document.getElementById("search_name").value;
+	rulebook_name = document.getElementById("rulebook_name").value;
+	lang_name = document.getElementById("lang_name").value;
+	source_name = document.getElementById("source_name").value;
+	source_detail = document.getElementById("source_detail").value;
+	if(pdf_name === '') {
+		alert('请输入gameid');
+		return
+	}
+	if(pageType === '') {
+		alert('请选择pageType');
+	}
+	pdf_query_url = 'http://180.76.244.130:3000/games/savePDFInfo';
+	$.ajax({
+		url: pdf_query_url,
+		data: {
+			pdf_name: pdf_name,
+			crop_len: crop_len,
+			search_name: search_name,
+			rulebook_name: rulebook_name,
+			lang_name: lang_name,
+			source_name: source_name,
+			source_detail: source_detail
+		},
+		success: function(data) {
+			console.log("savePDFInfo Success")
+		}
+	});
+}
+
 function click_submit_from(number) {
 	textURL = 'http://180.76.244.130:3000/games/getTextInfo';
 	temp_no = number + 1;
